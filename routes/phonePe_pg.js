a5611ed1ac4f1db3b9ce8b565a52f2fd44ce899b
Vasu Gupta <vasugupta1313@gmail.com>
2024-04-02 01:50:25 +0530

    redirect link test

--- routes/phonePe_pg.js
+++ routes/phonePe_pg.js
@@ -8,17 +8,16 @@ const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
 const merchantId = "PGTESTPAYUAT98";
 const saltKey = "bd5fd05b-506c-43a6-81b1-f7fd317bc71c";
 const saltIndex = 1;
-
+const merchantTransactionId = uniqid();
 phonePeRouter.post('/pay',auth, async (req, res) => {
     const payEndpoint = '/pg/v1/pay';
-    const merchantTransactionId = uniqid();
     const { id, amount } = req.body;
     const payLoad = {
         "merchantId": merchantId,
         "merchantTransactionId": merchantTransactionId,
         "merchantUserId": id,
         "amount": amount * 100, //in paise
-        "redirectUrl": `https://amazon-clone-server-vvb5.onrender.com/redirect-url/${merchantTransactionId}`,
+        "redirectUrl": `https://amazon-clone-server-vvb5.onrender.com/redirect-url/`,
         "redirectMode": "REDIRECT",
         "mobileNumber": "9999999999",
         "paymentInstrument": {
@@ -41,6 +40,7 @@ phonePeRouter.post('/pay',auth, async (req, res) => {
             request: base64EncodedPayload
         }
     };
+    console.log(merchantTransactionId);
     axios.request(options)
         .then(function (response) {
             console.log(response.data);
@@ -50,8 +50,8 @@ phonePeRouter.post('/pay',auth, async (req, res) => {
         });
 });
 
-phonePeRouter.get('/redirect-url/:merchantTransactionId', async (req, res) => { 
-    const { merchantTransactionId }= req.params;
+phonePeRouter.get('/redirect-url/', async (req, res) => { 
+    // const { merchantTransactionId }= req.params;
     const xVerify = sha256(`/pg/v1/status/${merchantId}/${merchantTransactionId}` + saltKey) + "###" + saltIndex;
     console.log(merchantTransactionId);
     if (merchantTransactionId) {  
@@ -64,8 +64,8 @@ phonePeRouter.get('/redirect-url/:merchantTransactionId', async (req, res) => {
               "X-VERIFY": xVerify,
               "X-MERCHANT-ID": merchantTransactionId,
       				},
-      
       };
+      console.log(merchantTransactionId);
       axios
         .request(options)
             .then(function (response) {
